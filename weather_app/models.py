d1cdef8c3348c65f15b39b05e61afd9748a59e94
Michał Madej <m.madejlodz@gmail.com>
2023-01-18 17:52:35 +0100

    drugi commit

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Weather(models.Model):
-    temperature = models.FloatField()
+    city = models.CharField(max_length=64, blank=False, null=False, default='city')
+    temp = models.FloatField()
+    pressure = models.PositiveSmallIntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True)
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
     path('', views.home2, name='home2'),
 ]

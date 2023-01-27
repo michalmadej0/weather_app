64cb4947ef43e3b93b39a2fb93fde11b1dd4455f
Michal <m.madejlodz@gmail.com>
2023-01-27 21:16:04 +0100

    feature data weather loader

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name ='weather_app'
+
 urlpatterns = [
     path('', views.home2, name='home2'),
+    path('load/', views.load_date, name='load'),
+
+
 ]
--- weather_app/views.py
+++ weather_app/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
+
 from datetime import datetime
 from babel.dates import format_datetime
 from .models import Weather
+import csv
 
 
 def home2(request):
-        if 'city2' in request.POST:
-            city2 = request.POST['city2']
+        if request.method == 'POST':
+            city2 = request.POST.get('city2')
         else:
             city2 = 'Łódź'
 
@@ -30,3 +32,34 @@ def home2(request):
                       {'temp': temp, 'description':description,
                        'icon':icon, 'city2':city2,'day':day, 'pressure':pressure})
 
+def load_date(request):
+    if request.method == 'POST':
+        weather_data = request.FILES.get('weather_data')
+        response = requests.get(weather_data, stream=True)
+        with open('temp/weather_data.csv', 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        with open('temp/weather_data.csv', newline='') as f:
+            csv_str = f.read()
+
+        # with open(weather_data, newline='') as f:
+        #     csv_str = f.read()
+
+            # reader = csv.DictReader(f)
+            # rows = [row for row in reader]
+
+        # temp_list = []
+        # pressure_list = []
+        # date_list = []
+        #
+        # for row in rows:
+        #     if row['key'] == 'temp':
+        #         temp_list.append(row['value'])
+        #     if row['key'] == 'pressure':
+        #         pressure_list.append(row['value'])
+        #     if row['key'] == 'date':
+        #         date_list.append(row['value'])
+
+        return render(request, 'weather_app/success.html', {'weather_data':weather_data, 'csv_str':csv_str})
+
+    return render(request, 'weather_app/load_data.html')
